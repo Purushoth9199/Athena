9ff91d3d79c4926744aef06e757cd9e72f78a3a2
purush <purushothaman619997@gmail.com>
2025-01-24 18:50:32 +0530

    Only deployment is pending

--- assets/js/qutiiz.js
+++ assets/js/qutiiz.js
@@ -203,10 +203,10 @@
           items: 2
         },
         1024: {
-          items: 3
+          items: 5
         },
         1200: {
-          items: 3
+          items: 5
         }
       }
     });
